45c1da440f6a68e5cf7fc5391d126bbb48ca826e
Monika Olejniczak <napior1115@wp.pl>
2021-06-08 16:37:52 +0200

    quiz form

---
+++ models/quiz.js
@@ -0,0 +1,12 @@
+// w tych modelach tworzymy modele w bazie danych w mongoDb 
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const quizSchema = new Schema({
+    title: { type: String, required: true },
+    vote: { type: Number, required: true, default: 0 },
+
+});
+
+module.exports = mongoose.model("Quiz", quizSchema);
